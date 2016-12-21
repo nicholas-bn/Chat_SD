ae4459020efbc6c0037caad6eb6782c174b82ba5
Barnini Nicholas <nicholas.barnini@gmail.com>
2016-12-21 16:32:15 +0100

    #9 Ajout de commentaires

--- src/Main/Chord.java
+++ src/Main/Chord.java
@@ -4,14 +4,23 @@ import java.util.ArrayList;
 
 public class Chord {
 
+	/** Taille de l'anneaux au départ */
 	public int sizeRing;
+	/** Liste des pairs */
 	public ArrayList<Pair> listPair;
 
+	/**
+	 * Constructeur de Chord avec un nombre de pairs
+	 * @param size - Nombre de Pairs à instancier
+	 */
 	public Chord(int size) {
 		this.sizeRing = size;
 		this.listPair = new ArrayList<Pair>();
 	}
 
+	/**
+	 * Méthode initialisabt l'anneau avec des pairs
+	 */
 	public void initialize() {
 		for (int i = 0; i < this.sizeRing; i++) {
 			this.listPair.add(new Pair("127.0.0.1", 8000+i));
