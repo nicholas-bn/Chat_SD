491842a4577324bc114027296b6c67a16af155d4
Thomas-VALVERDE <vt202704@etu.unice.fr>
2016-12-21 15:56:06 +0100

    #8 , ajout d'une méthode de comparaison d'ID

--- src/Main/Pair.java
+++ src/Main/Pair.java
@@ -5,23 +5,27 @@ import org.apache.commons.codec.digest.DigestUtils;
 import sun.applet.Main;
 
 public class Pair {
-	String identifiantNonHache;
-	String identifiantHache;
+	String identifiant;
 
 	public Pair(String identifiantNonHache) {
-		this.identifiantNonHache = identifiantNonHache;
+		this.identifiant = identifiantNonHache;
 	}
 
-	public String getHashSHA1(String toHash) {
-		return DigestUtils.sha1Hex(toHash);
+	public void HashachageId(String toHash) {
+		identifiant = DigestUtils.sha1Hex(toHash);
 	}
 
-	public void HasheIdentifiant(String identifiantNonHache) {
-		getHashSHA1(identifiantNonHache);
+	public static boolean CompareID(String idReferent, String identifant) {
+		if (idReferent.equals(identifant)) {
+			return true;
+		}
+		return false;
 	}
 
 	public static void main(String[] args) {
+		String idReferent = "4c38d8705ff98c8c1e89b6694b128122f63ee084";
 		Pair pair1 = new Pair("Michel");
-		System.out.println(pair1.getHashSHA1(pair1.identifiantNonHache));
+		pair1.HashachageId(pair1.identifiant);
+		CompareID(idReferent, pair1.identifiant);
 	}
 }
