76ebb8205260abf77b854a5a0a4a70436cfc1edf
Barnini Nicholas <nicholas.barnini@gmail.com>
2016-12-21 15:11:17 +0100

    #1 test de hashage

--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,11 @@
+import org.apache.commons.codec.digest.DigestUtils;
 
 public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("test");
 		System.out.println("Allo?");
-
+		System.out.println("Test de hash SHA-1: "+DigestUtils.sha1Hex("test1"));	
 	}
 
 }
