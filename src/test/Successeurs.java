8c1bb98d0f97288c09d4bfb3d11690cef9639a18
Karl Ferrero <karl.ferrero.kf@gmail.com>
2016-12-21 16:31:11 +0100

    #10 Création d'une méthode qui retourne le successeur le plus proche

--- src/test/Successeurs.java
+++ src/test/Successeurs.java
@@ -25,13 +25,40 @@ public class Successeurs {
 		return listSuccessors;
 	}
 
+	public static int findClosestSuccessor(int idSource, int idDest) {
+		// Liste de tous les successeurs :
+		ArrayList<Integer> listSuccessors = findSuccessors(idSource);
+
+		// Si l'id destination se trouve dans la liste des successeurs
+		if (listSuccessors.contains(idDest)) {
+			return idDest;
+		}
+
+		int idProche = idSource;
+
+		// On cherche le plus proche :
+		for (int id : listSuccessors) {
+			// Si l'id du successeur est plus proche (et inférieur à l'id dest)
+			if (id > idProche && id < idDest) {
+				idProche = id;
+			}
+		}
+		// L'id du pair le plus proche de l'id destination
+		return idProche;
+	}
+
 	public static void main(String[] args) {
 		System.out.println("0 : " + findSuccessors(0));
 		System.out.println("1 : " + findSuccessors(1));
 		System.out.println("2 : " + findSuccessors(2));
 		System.out.println("3 : " + findSuccessors(3));
 		System.out.println("6 : " + findSuccessors(6));
-		System.out.println("9 : " + findSuccessors(9));
+		System.out.println("9 : " + findSuccessors(9) + "\n");
+
+		//
+
+		System.out.println(findClosestSuccessor(0, 7));
+
 	}
 
 }
