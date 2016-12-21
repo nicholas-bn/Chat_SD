a34f3f9c01bfb1916ad005bc026ee3c4d8831fb7
Thomas-VALVERDE <vt202704@etu.unice.fr>
2016-12-21 16:02:34 +0100

    #8 , tests de la methode CompareID

--- src/Main/Pair.java
+++ src/Main/Pair.java
@@ -23,9 +23,13 @@ public class Pair {
 	}
 
 	public static void main(String[] args) {
+		// On cree un ID referent qui est egal a l'ID du 1er pair
 		String idReferent = "4c38d8705ff98c8c1e89b6694b128122f63ee084";
+		// On cree un autre ID qui est different
+		String autreID = "4c38d8705ff98c8c1e89b6694b1281e22f63ee084";
 		Pair pair1 = new Pair("Michel");
 		pair1.HashachageId(pair1.identifiant);
 		System.out.println(pair1.CompareID(idReferent));
+		System.out.println(pair1.CompareID(autreID));
 	}
 }
