71ac06eae1ae6672a991678fd1f0b76e64e6f3a1
Thomas-VALVERDE <vt202704@etu.unice.fr>
2016-12-21 15:33:34 +0100

    #8 , création de la méthode de Hachage

--- src/Main/Pair.java
+++ src/Main/Pair.java
@@ -1,19 +1,27 @@
 package Main;
+
 import org.apache.commons.codec.digest.DigestUtils;
 
 import sun.applet.Main;
+
 public class Pair {
-	
-	 String identifiantHache;
-	
-	public Pair(String identifiantNonHache)
-	{
-		this.identifiantHache = DigestUtils.sha1Hex(identifiantNonHache);
+	String identifiantNonHache;
+	String identifiantHache;
+
+	public Pair(String identifiantNonHache) {
+		this.identifiantNonHache = identifiantNonHache;
+	}
+
+	public String getHashSHA1(String toHash) {
+		return DigestUtils.sha1Hex(toHash);
+	}
+
+	public void HasheIdentifiant(String identifiantNonHache) {
+		getHashSHA1(identifiantNonHache);
 	}
 
 	public static void main(String[] args) {
 		Pair pair1 = new Pair("Michel");
-		
-		System.out.println(pair1.toString());
+		System.out.println(pair1.getHashSHA1(pair1.identifiantNonHache));
 	}
 }
