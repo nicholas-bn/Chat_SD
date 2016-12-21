eeaad093502b5d1be9fedc9e0b031444c5a12524
Thomas-VALVERDE <vt202704@etu.unice.fr>
2016-12-21 16:33:41 +0100

    #11 , passage des attributs en private + accesseurs + rajout de l'attribut nud

--- src/Main/Pair.java
+++ src/Main/Pair.java
@@ -5,11 +5,12 @@ import org.apache.commons.codec.digest.DigestUtils;
 import sun.applet.Main;
 
 public class Pair {
-	String ip;
-	int port;
-	String identifiant;
+	private int numero;
+	private String ip;
+	private int port;
+	private String identifiant;
 
-	public Pair(String ip, int port) {
+	public Pair(int numero,String ip, int port) {
 		this.ip = ip;
 		this.port = port;
 		identifiant = (this.ip + ":" + this.port);
@@ -52,10 +53,10 @@ public class Pair {
 
 	public static void main(String[] args) {
 		// On cree un ID referent qui est egal a l'ID du 1er pair
-		String idReferent = "e98b1de3c972ca554569b28357c407c807690e13";
+		String idReferent = "0210df6cd869c217af4ac03664c330d9c9841926";
 		// On cree un autre ID qui est different
 		String autreID = "4c38d8705ff98c8c1e89b6694b1281e22f63ee084";
-		Pair pair1 = new Pair("1270101", 88);
+		Pair pair1 = new Pair(1,"127.01.01", 88);
 		//On observe l'identifiant cree avant hashage
 		System.out.println("Identifiant avant hashage: "+ pair1.getIdentifiant());
 		// On hache l'identifiant
@@ -63,5 +64,13 @@ public class Pair {
 		System.out.println("Identifiant apres hashage: "+ pair1.getIdentifiant());
 		System.out.println(pair1.CompareID(idReferent));
 		System.out.println(pair1.CompareID(autreID));
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
 	}
 }
