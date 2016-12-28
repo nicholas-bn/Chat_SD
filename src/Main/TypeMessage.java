a1147aa51e24de049f434a66c9c8051ebc78a0fe
Barnini Nicholas <nicholas.barnini@gmail.com>
2016-12-28 15:44:02 +0100

    #15 Ajout d'un enum pour différencier les types de messages

---
+++ src/Main/TypeMessage.java
@@ -0,0 +1,11 @@
+package Main;
+
+/**
+ * @author Barnini Nicholas
+ * 
+ * Enumération permettant de différiencer les différents types de messages.
+ *
+ */
+public enum TypeMessage {
+	MessageChat, AjoutPair, SuppressionPair;
+}
