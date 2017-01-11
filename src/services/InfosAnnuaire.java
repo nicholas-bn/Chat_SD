4b05b72f8d89eb8c8d9efbaab2dea963244e9851
Karl Ferrero <karl.ferrero.kf@gmail.com>
2017-01-11 15:08:21 +0100

    Allo

---
+++ src/services/InfosAnnuaire.java
@@ -0,0 +1,9 @@
+package services;
+
+public abstract class InfosAnnuaire {
+	/** IP de l'annuaire */
+	public static final String ip = "127.0.0.1";
+
+	/** PORT de l'annuaire */
+	public static final int port = 8000;
+}
